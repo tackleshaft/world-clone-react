ad2d30a9b1dce7c0f6c1d009fde9c29b9e24f16f
Hugh Stapleton <hstapleton808@gmail.com>
2024-03-17 17:06:29 -0400

    keyboard registers letters

--- wordle-clone/src/components/Keyboard.jsx
+++ wordle-clone/src/components/Keyboard.jsx
@@ -1,18 +1,35 @@
-import React, {useCallback, useEffect} from "react";
+import React, {useCallback, useEffect, useContext} from "react";
 import Key from "./Key";
+import { AppContext } from "../App";
 
 function Keyboard () {
+    const { onEnter, onDelete, onSelectLetter } = useContext(AppContext)
+
     const keys1 = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"];
     const keys2 = ["A", "S", "D", "F", "G", "H", "J", "K", "L"];
     const keys3 = ["Z", "X", "C", "V", "B", "N", "M"];
 
     const handleKeyboard = useCallback((event) => {
         if (event.key === "Enter") {
-
+            onEnter();
         } else if (event.key === "Backspace") {
-
+            onDelete();
         } else {
-            
+            keys1.forEach((key) => {
+                if (event.key.toLowerCase() === key.toLowerCase()) {
+                    onSelectLetter(key)
+                }
+            })
+            keys2.forEach((key) => {
+                if (event.key.toLowerCase() === key.toLowerCase()) {
+                    onSelectLetter(key)
+                }
+            })
+            keys3.forEach((key) => {
+                if (event.key.toLowerCase() === key.toLowerCase()) {
+                    onSelectLetter(key)
+                }
+            })
         }
     })
 
